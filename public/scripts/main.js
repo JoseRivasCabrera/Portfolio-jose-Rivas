b5e3329c5f03e2e1e463d4b47faf79f904fe586a
rivas.dev.contacto <rivas.dev.contacto@gmail.com>
2024-04-01 20:58:33 +0000

    se crea el menu plegable con js y con sus estilos

---
+++ public/scripts/main.js
@@ -0,0 +1,43 @@
+function cambiarHeader() {
+    let contenedorMenu = document.getElementById('contenedor--menu');
+    let contenidoMenu = '';
+
+    if (window.innerWidth <= 768) {
+        contenidoMenu = `<nav class="navbar">
+        <h1 class="tittle--name">Rivas</h1>
+
+        <label class="labe_hamburguesa" for="menu_hamburguesa">
+            <img src="public/img/iconos/menu.svg" alt="Menu" class="icono--menu">
+        </label>
+
+        <input class="menu_hamburguesa" type="checkbox" name="" id="menu_hamburguesa">
+
+        <ul class="ul_links">
+            <li class="li_links">
+                <a href="#" class="link">Inicio</a>
+            </li>
+            <li class="li_links">
+                <a href="#" class="link">Sobre mi</a>
+            </li>
+            <li class="li_links">
+                <a href="#" class="link">Productos</a>
+            </li>
+        </ul>
+    </nav>`;
+    } else {
+        contenidoMenu = `<div class="header--elements">
+        <h1 class="tittle--name">Rivas</h1>
+        <nav class="header--menu">
+          <a href="menu-plegable.html" class="element--menu"><p>Inicio</p></a>
+          <a href="" class="element--menu"><p>Acerca de mi</p></a>
+          <a href="" class="element--menu"><p>Proyectos</p></a>
+        </nav>
+        <button class="btn--contacto">Contáctame</button>
+      </div>`
+    }
+    contenedorMenu.innerHTML = contenidoMenu;
+}
+
+window.onload = cambiarHeader;
+window.onresize = cambiarHeader;
+
