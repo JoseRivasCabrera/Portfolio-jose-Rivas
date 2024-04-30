fa43fc59b576b50278914d8a90fe3aca6fbc223f
Rivas <jose.trabajo877@gmail.com>
2024-04-29 21:43:10 -0600

    se agregan las cards de servicio canva plata y oro

--- public/scripts/linkbio.js
+++ public/scripts/linkbio.js
@@ -1,6 +1,9 @@
-const servicios = document.getElementById("seleccion-navar-izq");
-const info = document.getElementById("seleccion-navar-der");
+const cajaJervicios = document.getElementById("seleccion-navar-izq");
+const cajaInfo = document.getElementById("seleccion-navar-der");
 const contenedorDelContenido = document.getElementById("contenedor-contenido");
+const servicios = document.getElementById("servicios");
+const informacion = document.getElementById("informacion");
+
 const contenidoInfo = `<div class="contenedor-elementos-info">
 
 <a href="https://tinyurl.com/discord-ticoDev" class="elemento-info link-url">
@@ -13,11 +16,6 @@ const contenidoInfo = `<div class="contenedor-elementos-info">
   <i class='bx bx-right-arrow-alt bx-flip-vertical flecha'></i>
 </a>
 
-<a href="mailto:rivas_web-developer@outlook.com" class="elemento-info link-url">
-  <p class="nombre-link">Mail</p>
-  <i class='bx bx-right-arrow-alt bx-flip-vertical flecha'></i>
-</a>
-
 <a href="#" class="elemento-info link-url">
   <p class="nombre-link">Telegram</p>
   <i class='bx bx-right-arrow-alt bx-flip-vertical flecha'></i>
@@ -25,7 +23,7 @@ const contenidoInfo = `<div class="contenedor-elementos-info">
 
 <div class="elemento-info ">
   <p class="nombre-link">Portafolio</p>
-  <a href="#" class="link-url">
+  <a href="https://tinyurl.com/Rivasportfolio" class="link-url">
     <i class='bx bx-right-arrow-alt bx-flip-vertical flecha'></i>
   </a>
 </div>
@@ -47,7 +45,7 @@ const contenidoServicios = `<section class="contenedor-elementos-servicio">
       <img class="img-servicio" src="public/img/image.png" alt="">
     </div>
     <div class="contenedor-textos-servicio">
-      <H1 class="titulo-servicio">LinkTree</H1>
+      <H1 class="titulo-prodcuto">LinkTree</H1>
       <h2 class="descripcion-servicio">Creo el servicio de links ideal para tu negocio</h2>
     </div>
     </div>
@@ -55,24 +53,28 @@ const contenidoServicios = `<section class="contenedor-elementos-servicio">
 
 contenedorDelContenido.innerHTML = contenidoInfo;
 
-info.classList.toggle("navar-selecion");
+cajaInfo.classList.toggle("navar-selecion");
 
 valorClick = 0;
-servicios.addEventListener("click", function eventos(event) {
+cajaJervicios.addEventListener("click", function eventos(event) {
 
   if (valorClick == 0) {
-    servicios.classList.toggle("navar-selecion");
-    info.classList.toggle("navar-selecion");
+    cajaJervicios.classList.toggle("navar-selecion");
+    cajaInfo.classList.toggle("navar-selecion");
+    servicios.classList.toggle("navar-text-color");
+    informacion.classList.toggle("navar-text-color");
     contenedorDelContenido.innerHTML = contenidoServicios;
     valorClick = 1;
   }
 
 });
 
-info.addEventListener("click", function eventos(event) {
+cajaInfo.addEventListener("click", function eventos(event) {
   if (valorClick == 1) {
-    info.classList.toggle("navar-selecion");
-    servicios.classList.toggle("navar-selecion");
+    cajaInfo.classList.toggle("navar-selecion");
+    cajaJervicios.classList.toggle("navar-selecion");
+    servicios.classList.toggle("navar-text-color");
+    informacion.classList.toggle("navar-text-color");
     contenedorDelContenido.innerHTML = contenidoInfo;
     valorClick = 0;
   }
